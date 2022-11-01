5f7f3588f872fce1f906fe7a66a3bb465469d44b
Prakash Sewani <81285705+PrakashSewani@users.noreply.github.com>
2022-11-01 16:09:59 +0530

    added a guided authentication system

---
+++ OAuthProject/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using OAuthProject.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OAuthProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("Index")]
+        public ActionResult Index()
+        {
+            ActionResult returnResult = GetInvalidResult();
+
+            string code = Request.Query["code"];
+            if (code != null)
+            {
+                GetUserDetailsAsync(code);
+                var redirectURI = _configuration["SingleSignOn:Redirect_uri"];
+
+                returnResult = Redirect(redirectURI);
+            }
+            return returnResult;
+        }
+
+        public async Task GetUserDetailsAsync(string code)
+        {
+            if (!string.IsNullOrEmpty(code))
+            {
+                HttpResponseMessage response = GetResponseFromGetOAuthTokenURL(code);
+                OAuthToken oAuthToken = new OAuthToken();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    oAuthToken = JsonConvert.DeserializeObject<OAuthToken>(responseBody);
+
+                    List<string> decodedIDToken = DecodeIDToken(oAuthToken);
+
+                    string accessToken = decodedIDToken[0];
+                    string refreshToken = decodedIDToken[1];
+                    string email = decodedIDToken[2];
+                }
+            }
+        }
+
+        private List<string> DecodeIDToken(OAuthToken oAuthToken)
+        {
+            string accessToken = oAuthToken.AccessToken;
+            string refreshToken = oAuthToken.RefreshToken;
+
+            string stream = oAuthToken.IDToken;
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = jsonToken as JwtSecurityToken;
+
+            string email = tokenS.Claims.First(claim => claim.Type == "email").Value;
+
+            List<string> decodedList = new List<string>();
+            decodedList.Add(accessToken);
+            decodedList.Add(refreshToken);
+            decodedList.Add(email);
+
+            return decodedList;
+        }
+
+
+        private HttpResponseMessage GetResponseFromGetOAuthTokenURL(string code)
+        {
+            var url = _configuration["SingleSignOn:Access_Token_Url"];
+
+            var grantType = _configuration["SingleSignOn:GrantType"];
+            var clientID = _configuration["SingleSignOn:ClientId"];
+            var clientSecret = _configuration["SingleSignOn:ClientSecret"];
+            var redirectURI = _configuration["SingleSignOn:Redirect_uri"];
+
+            var parameters = new Dictionary<string, string> { { "grant_type", grantType }, { "client_id", clientID }, { "client_secret", clientSecret }, { "code", code }, { "redirect_uri", redirectURI } };
+            var encodedContent = new FormUrlEncodedContent(parameters);
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = client.PostAsync(url, encodedContent).Result;
+            return response;
+        }
+
+        ActionResult GetInvalidResult()
+        {
+            string redirectPage = "LogoutRedirect";
+            return View(redirectPage);
+        }
+    }
+}
---
+++ OAuthProject/Models/OAuthToken.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthProject.Models
+{
+    public class OAuthToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonProperty("sfdc_community_url")]
+        public string SfdcCommunityUrl { get; set; }
+
+        [JsonProperty("sfdc_community_id")]
+        public string SfdcCommunityId { get; set; }
+
+        [JsonProperty("signature")]
+        public string Signature { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("id_token")]
+        public string IDToken { get; set; }
+
+        [JsonProperty("instance_url")]
+        public string InstanceUrl { get; set; }
+
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("issued_at")]
+        public string IssuedAt { get; set; }
+
+        public string Email { get; set; }
+
+        public string SESAID { get; set; }
+    }
+}
